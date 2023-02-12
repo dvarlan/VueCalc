ea45bb8a1cd4a27c32aed035ed1ea2907683b23f
Dani <daniel.varlan20@gmail.com>
2023-02-12 14:56:02 +0100

    Added basic logic

--- src/components/CalculatorComponent.vue
+++ src/components/CalculatorComponent.vue
@@ -4,10 +4,10 @@
       {{ currentVal || 0 }}
     </div>
     <div class="buttons">
-      <button v-for="(btn, i) in buttons" :key="i">{{btn}}</button>
+      <button v-for="(btn, i) in buttons" :key="i" @click="onButtonPress(btn)">{{btn}}</button>
     </div>
-    <div class="pastResults">
-      Verlauf: {{ pastResults }}
+    <div class="pastResults" v-if="pastResults.length != 0">
+      Past results: {{ pastResults }}
     </div>
   </div>
 </template>
@@ -18,13 +18,49 @@ export default {
   data() {
     return {
       currentVal: '',
+      result: '',
       buttons: 
       ['C', 'CE', '+', '-',
        '7', '8', '9', '*',
        '4', '5', '6', '/',
        '1', '2', '3', '=',
        '0', '.', '%'],
-      pastResults: ''
+      pastResults: []
+    }
+  },
+  methods: {
+    onButtonPress (btn) {
+
+      if (btn === 'C') {
+        this.currentVal = ''
+      }
+
+      if (btn === 'CE') {
+        this.currentVal = ''
+        this.pastResults = []
+      }
+
+      if (btn === '=') {
+        try {
+          this.result = eval(this.currentVal)
+          this.appendPastResults(this.result)
+          this.currentVal = this.result
+        } catch (error) {
+          this.currentVal = 'ERROR!'
+        }
+      }
+
+      if (!['=', 'C', 'CE'].includes(btn)) {
+        this.currentVal += btn
+      }
+    },
+    appendPastResults(res) {
+      if (this.pastResults.length <= 5) {
+        this.pastResults.push(res)
+      } else {
+        this.pastResults.shift()
+        this.pastResults.push(res)
+      }
     }
   }
 }
